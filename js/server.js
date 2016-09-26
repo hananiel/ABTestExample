41a79c25df8de34f21d8235951f990f76411fed0
SARELLA, HANANIEL <hananiel.sarella@kroger.com>
2016-09-26 19:26:56 -0400

    fix es6 issue in server.js

--- js/server.js
+++ js/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('.'));
-let PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 3001;
 app.listen( PORT , function () {
   console.log('Example app listening on port '+PORT+'!');
 });
